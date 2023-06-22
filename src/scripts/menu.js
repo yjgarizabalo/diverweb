d581ede906353f2ca63dda688b82a9f5c008ec35
Yair G <yjgarizabalo@gmail.com>
2023-06-21 22:15:28 -0500

    [diverweb] add nav overlay

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,5 +1,29 @@
 const hamburgerIcon = document.querySelector('.nav__hamburguer');
+const navOverlay = document.querySelector('.nav__overlay')
 
 hamburgerIcon.addEventListener('click', () => {
     hamburgerIcon.classList.toggle('nav__hamburguer--open');
+
+    navOverlay.classList.toggle('nav__overlay--show');
 });
+
+navOverlay.addEventListener('click', (e) => {
+    e.preventDefault();
+    const currentElement = e.target;
+    
+    if(isActive(currentElement, 'nav__parent')) {
+			const  subMenu = currentElement.parentElement.children[1];
+			if(window.innerWidth < 768) {
+				let height = (subMenu.clientHeight == 0) 
+										? subMenu.scrollHeight : 0;
+
+				subMenu.style.height = `${height}px`;
+			}else {
+
+			}
+    }
+})
+
+function isActive(element, string) {
+    return element.classList.value.includes(string)
+}
