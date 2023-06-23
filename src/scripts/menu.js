ce2711c59b3540f019de9ea58440c4157ad9113b
yairg <yjgarizabalo@gmail.com>
2023-06-23 16:29:01 -0500

    [diverweb] add

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,29 +1,63 @@
-const hamburgerIcon = document.querySelector('.nav__hamburguer');
-const navOverlay = document.querySelector('.nav__overlay')
+const hamburguerIcon = document.querySelector('.nav__hamburguer');
+const navOverlay = document.querySelector('.nav__overlay');
+let currentDropdown = navOverlay;
 
-hamburgerIcon.addEventListener('click', () => {
-    hamburgerIcon.classList.toggle('nav__hamburguer--open');
+hamburguerIcon.addEventListener('click', ()=>{
+    hamburguerIcon.classList.toggle('nav__hamburguer--open');
 
     navOverlay.classList.toggle('nav__overlay--show');
 });
 
-navOverlay.addEventListener('click', (e) => {
+navOverlay.addEventListener('click', (e)=>{
     e.preventDefault();
     const currentElement = e.target;
-    
-    if(isActive(currentElement, 'nav__parent')) {
-			const  subMenu = currentElement.parentElement.children[1];
-			if(window.innerWidth < 768) {
-				let height = (subMenu.clientHeight == 0) 
-										? subMenu.scrollHeight : 0;
+    // console.log(e.target.classList.value);
 
-				subMenu.style.height = `${height}px`;
-			}else {
+    if( isActive(currentElement, 'nav__parent') ){
 
-			}
+        const subMenu = currentElement.parentElement.children[1];
+        console.log(subMenu);
+
+        if(window.innerWidth < 768){
+
+            let height = (subMenu.clientHeight == 0) 
+                        ? subMenu.scrollHeight : 0;
+            // console.log(subMenu.clientHeight);
+
+            subMenu.style.height = `${height}px`;
+        }else{
+
+            if( !isActive(subMenu, 'nav__inner--show') ){
+                closeDropdown(currentDropdown);
+            }
+            subMenu.classList.toggle('nav__inner--show');
+
+            currentDropdown = subMenu;
+
+        }
+
     }
-})
+    
+});
 
-function isActive(element, string) {
-    return element.classList.value.includes(string)
+function isActive(element, string){
+    return element.classList.value.includes(string);
 }
+
+function closeDropdown(currentDropdown){
+    if( isActive(currentDropdown, 'nav__inner--show')){
+        currentDropdown.classList.remove('nav__inner--show');
+    }
+}
+
+window.addEventListener('resize', ()=>{
+    closeDropdown(currentDropdown);
+
+    if(window.innerWidth > 768){
+        const navInners = document.querySelectorAll('.nav__inner');
+
+        navInners.forEach(navInner =>{
+            navInner.style.height = '';
+        });
+    }
+});
